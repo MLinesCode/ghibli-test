88b349c641010e10ee9f57a443fee72927fb8f64
Miguel Garcia <mikexx1264@gmail.com>
2022-03-29 23:48:54 -0600

    Se realizaron arreglos menores en Header - HomeScreen y estilos en recommendedmovie

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import IconLite from '../components/iconlite/IconLite'
-import IconClock from '../assets/icons/clock.png'
-import AvatarUser from '../assets/avatar.png'
-import LogoGhibli from '../assets/logo-ghibli.png'
-import './header.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import IconLite from "../components/iconlite/IconLite";
+import IconClock from "../assets/icons/clock.png";
+import AvatarUser from "../assets/avatar.png";
+import LogoGhibli from "../assets/logo-ghibli.png";
+import "./header.css";
 
 const Header = () => {
   return (
-    <div className="header__container">
+    <header className="header__container">
       <div className="header__icons">
         <NavLink
           activeClassName="active"
           className="header__link"
           exact
-          to='./profile'
+          to="./profile"
         >
-          <IconLite iconSource={ AvatarUser } />
+          <IconLite iconSource={AvatarUser} />
         </NavLink>
-        <IconLite iconSource={ IconClock } />
+        <IconLite iconSource={IconClock} />
       </div>
       <figure className="header__logo">
-        <img src={ LogoGhibli } alt={ LogoGhibli } />
+        <img src={LogoGhibli} alt={LogoGhibli} />
       </figure>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/views/home/HomeScreen.js
+++ src/views/home/HomeScreen.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import Carousel from '../../components/carousel/Carousel'
-import Menu from '../../components/menu/Menu'
-import RecommendedMovie from '../../components/recommendedmovie/RecommendedMovie'
-import Footer from '../../layout/Footer'
-import Header from '../../layout/Header'
-import CoverImage from '../../assets/main_movie.jpg'
-import './home.css'
+import React from "react";
+import Carousel from "../../components/carousel/Carousel";
+import Menu from "../../components/menu/Menu";
+import RecommendedMovie from "../../components/recommendedmovie/RecommendedMovie";
+import Footer from "../../layout/Footer";
+import Header from "../../layout/Header";
+import CoverImage from "../../assets/main_movie.jpg";
+import "./home.css";
 
 const HomeScreen = () => {
   return (
     <div className="home__container">
-      <Header redirectTo={ '/login' } />
-      <RecommendedMovie movieCover={ CoverImage } alt={ CoverImage } movieTitle="El castillo del vagabundo" />
+      <Header redirectTo={"/login"} />
+      <RecommendedMovie
+        movieCover={CoverImage}
+        alt={CoverImage}
+        movieTitle="El castillo del vagabundo"
+      />
       <Carousel category="Mi lista" />
       <Carousel category="Lo mas visto" />
       <Carousel category="Ganadoras del Oscar" />
       <Menu />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
