c6e0a5d2111b166ce42797c05e87b7aa7c0d5b94
Miguel Garcia <mikexx1264@gmail.com>
2022-06-26 15:16:07 -0500

    Se agregaron los banner de las peliculas

--- src/components/movieposter/MoviePoster.js
+++ src/components/movieposter/MoviePoster.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PosterImage from '../../assets/poster.jpg'
 import './movieposter.css';
 
-const MoviePoster = ({ title, poster, id }) => {
+const MoviePoster = ({ title, image, id }) => {
   return (
-      <div className="movie__container">
-        <Link
-          to="/movie"
-        >
-          <figure className="movie__figure">
-            <img className="movie__image" src={ PosterImage } loading="lazy" alt={ title } />
-          </figure>
-        </Link>
-        <p className="movie__title">{ title }</p>
-      </div>
+    <div className="movie__container">
+      <Link
+        to="/movie"
+      >
+        <figure className="movie__figure">
+          <img className="movie__image" src={image} loading="lazy" alt={title} />
+        </figure>
+      </Link>
+      <p className="movie__title">{title}</p>
+    </div>
   )
 }
 
